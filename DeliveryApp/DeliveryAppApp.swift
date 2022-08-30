0aab74b18adc9b23918bbab1925d48b27c67b048
AntonUsov <anton.usov@stm-labs.ru>
2022-08-30 19:20:09 +0300

    Initial Commit

---
+++ DeliveryApp/DeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryAppApp.swift
+//  DeliveryApp
+//
+//  Created by Антон Усов on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
